293ddba8f491f645f0c370eb2a2eb0dae02db54c
Lihui Hu <lihui.hu@nih.gov>
2025-11-04 14:09:29 -0500

    Initial commit with current code

---
+++ gsrs_core/API_testsuits/scripts/structure_search.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import csv
+from test_config import headerspost, SERVER_URL, input_path, output_path, verify_flag
+
+
+csv_file_path = input_path + 'sample_data.csv'
+base_url = SERVER_URL + "api/v1/substances/interpretStructure"
+base_url2 = SERVER_URL + "api/v1/substances/structureSearch"
+with open(csv_file_path, mode='r') as file:
+    csv_reader = csv.DictReader(file)      
+    for index, row in enumerate(csv_reader, start=1):
+        data = row['smiles']
+        query_params1 = {}
+        response = requests.post(base_url, headers=headerspost, params=query_params1, data=data, verify=verify_flag)
+        if response.status_code == 200:
+            print(data, f"Request {index} was successful!")
+            response_dict = json.loads(response.text)
+            structure = response_dict.get("structure")
+            if structure:
+                ida = structure.get('id')
+                print(f"ID for request {index}: {ida}")
+            else:
+                print(f"No structure found for request {index}")
+        else:
+            print(f"Error {index}:", response.status_code, response.text)
+            continue  
+        query_params = {
+            "q": ida,
+            "type": "structuresearch",
+            "smiles": data
+        }
+        response1 = requests.get(base_url2, headers=headerspost, params=query_params, data=data, verify=verify_flag)
+        
+        if response1.status_code == 200:
+            print(f"Request {index} (GET) was successful!", data)
+            response1_dict = json.loads(response1.text)
+            print(type(response1_dict), response1_dict['finished'])
+            print(f"Response1 for request {index}: {response1_dict}")
+        else:
+            print(f"Error {index} (GET):", response1.status_code, response1.text)
+
+print("All requests completed.")
---
+++ gsrs_products/uploader.py
@@ -0,0 +1,58 @@
+import multiprocessing
+import requests
+import xml_parser
+import time
+from datetime import datetime
+
+def upload_data(filename, data, url, headers):
+    try:
+        response = requests.post(url, json=data, headers=headers, verify=False)
+        if response.status_code == 200:
+            print(f"[INFO] Uploaded: {filename}")
+        else:
+            print(f"[WARNING] Failed to upload {filename}. Status Code: {response.status_code}")
+    except Exception as e:
+        print(f"[ERROR] Exception occurred during upload of {filename}: {e}")
+
+
+def upload_files_in_batches(data_list, batch_size, url, headers):
+    print("[INFO] Starting batch upload of data...")
+
+    # Create a pool of workers based on the number of CPU cores
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        for i in range(0, len(data_list), batch_size):
+            batch_data = data_list[i:i + batch_size]
+            for index, data in enumerate(batch_data):
+                try:
+                    if data is not None:
+                        filename = f"data_{i + index + 1}.json"
+                        pool.apply_async(upload_data, args=(filename, data, url, headers))
+                    else:
+                        print(f"[WARNING] Skipping upload for data index {i + index + 1} because it is None")
+                except Exception as e:
+                    print(f"[ERROR] Exception occurred while preparing upload for data index {i + index + 1}: {e}")
+
+        
+        pool.close()
+        pool.join()
+
+    print("[INFO] Completed batch upload.")
+
+if __name__ == "__main__":
+    batch_size = 50
+    urlPROD = 'https://gsrs.fda.gov/api/v1/products'  
+    #headersPROD = {} #headers via seperate file
+    try:
+        
+        data_zip_path = 'C:\\Users\\Arunasri.Nishtala\\Desktop\\Products\\SPLxml7-11-24\\only_xmls\\data9_productlevel_123k2.zip'
+        parsed_data = xml_parser.load_data_from_zip(data_zip_path)
+
+        # Check if parsed_data is empty or None
+        if not parsed_data:
+            print("[ERROR] No data available to upload. Please check the data.zip file.")
+        else:
+            # Upload data in batches
+            upload_files_in_batches(parsed_data, batch_size, urlPROD, headersPROD)
+    except Exception as e:
+        print(f"[ERROR] An error occurred in the main upload process: {e}")
+
